c49401dac254bf700ab4e6c64eeba12c01f81cd4
Ntando Khanyile <khanyilesiyanda01@gmail.com>
2025-11-10 07:53:59 +0200

    Readme added

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     const string hr = "---------------------------------------------";
-    cout << os_name << "\n\n\n\n\n";
+    cout << username << "\n\n\n\n\n";
     return EXIT_SUCCESS;
 }
